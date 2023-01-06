96fd79e35744a890edc712aed058ad51201ebd83
Tony <bowen.anth@gmail.com>
2023-01-06 05:54:38 -0700

    updated urls in app.js to heroku

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch('http://localhost:3001/imageurl', {
+    fetch('https://intense-plains-82472.herokuapp.com/imageurl', {
       method: 'post',
       headers:{'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -77,7 +77,7 @@ class App extends Component {
     .then(response => response.json())
     .then(response => {
       if (response) {
-        fetch('http://localhost:3001/image', {
+        fetch('https://intense-plains-82472.herokuapp.com/image', {
           method: 'put',
           headers:{'Content-Type': 'application/json'},
           body: JSON.stringify({
