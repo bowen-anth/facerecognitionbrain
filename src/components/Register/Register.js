51ea2d34d68cb1a2848b73e37bd61377fc2b8618
Tony <bowen.anth@gmail.com>
2023-01-06 08:14:09 -0700

    changed signin and reg urls

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('https://smart-brain-frontend.herokuapp.com/register', {
+        fetch('https://intense-plains-82472.herokuapp.com/register', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('https://smart-brain-frontend.herokuapp.com/signin', {
+        fetch('https://intense-plains-82472.herokuapp.com/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
