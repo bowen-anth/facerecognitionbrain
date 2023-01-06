1f8d138b907b52cec05778d74c69afebb3127cfc
Tony <bowen.anth@gmail.com>
2023-01-06 07:13:57 -0700

    updated heroku url

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch('smart-brain-frontend.herokuapp.com/imageurl', {
+    fetch('https://intense-plains-82472.herokuapp.com/imageurl', {
       method: 'post',
       headers:{'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -77,7 +77,7 @@ class App extends Component {
     .then(response => response.json())
     .then(response => {
       if (response) {
-        fetch('smart-brain-frontend.herokuapp.com/image', {
+        fetch('https://intense-plains-82472.herokuapp.com/image', {
           method: 'put',
           headers:{'Content-Type': 'application/json'},
           body: JSON.stringify({
