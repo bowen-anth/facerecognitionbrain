0bd2cf4468568318083a6ea2149967b4f8b8ede5
Tony <bowen.anth@gmail.com>
2023-01-06 17:36:56 -0700

    andrei btn submit info in app.js

--- src/App.js
+++ src/App.js
@@ -67,33 +67,64 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch('https://intense-plains-82472.herokuapp.com/imageurl', {
-      method: 'post',
-      headers:{'Content-Type': 'application/json'},
-      body: JSON.stringify({
-        input: this.state.input
-      })
-    })
-    .then(response => response.json())
-    .then(response => {
-      if (response) {
-        fetch('https://intense-plains-82472.herokuapp.com/image', {
-          method: 'put',
-          headers:{'Content-Type': 'application/json'},
-          body: JSON.stringify({
-            id: this.state.user.id
-          })
+      fetch('https://intense-plains-82472.herokuapp.com/imageurl', {
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+          input: this.state.input
         })
-        .then(response => response.json())
-        .then(count => {
-          this.setState(Object.assign(this.state.user, { entries: count }))
+      })
+      .then(response => response.json())
+      .then(response => {
+        if (response) {
+          fetch('https://intense-plains-82472.herokuapp.com/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
           })
-          .catch(console.log);
-      }
-      this.displayFaceBox(this.calculateFaceLocation(response)) 
-    })
-    .catch(err => console.log(err));
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, { entries: count}))
+            })
+            .catch(console.log)
+
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
+      .catch(err => console.log(err));
   }
+
+  // onButtonSubmit = () => {
+  //   this.setState({imageUrl: this.state.input});
+  //   fetch('https://intense-plains-82472.herokuapp.com/imageurl', {
+  //     method: 'post',
+  //     headers:{'Content-Type': 'application/json'},
+  //     body: JSON.stringify({
+  //       input: this.state.input
+  //     })
+  //   })
+  //   .then(response => response.json())
+  //   .then(response => {
+  //     if (response) {
+  //       fetch('https://intense-plains-82472.herokuapp.com/image', {
+  //         method: 'put',
+  //         headers:{'Content-Type': 'application/json'},
+  //         body: JSON.stringify({
+  //           id: this.state.user.id
+  //         })
+  //       })
+  //       .then(response => response.json())
+  //       .then(count => {
+  //         this.setState(Object.assign(this.state.user, { entries: count }))
+  //         })
+  //         .catch(console.log);
+  //     }
+  //     this.displayFaceBox(this.calculateFaceLocation(response)) 
+  //   })
+  //   .catch(err => console.log(err));
+  // }
 
 onRouteChange = (route) => {
   if (route === 'signout') {
