f422790e92db52fef555e754ce9b1bb4650abc4a
Tony <bowen.anth@gmail.com>
2023-01-06 17:55:37 -0700

    reverted some changes

--- src/App.js
+++ src/App.js
@@ -66,85 +66,85 @@ class App extends Component {
   }
 
 //stackoverflow's
-  onButtonSubmit = () => {
-    console.log('button clicked');
-    this.setState({imageUrl: this.state.input});
-    const USER_ID = 'oxxxxxxxxxx';//(the code by your name)
-    const PAT = '96d20xxxxxxxxxxxx';//(your Clarifai api key)
-    const APP_ID = 'facerec';//(what you named your app in Clarifai)
-    const MODEL_ID = 'face-detection';
-    const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';    
-    const IMAGE_URL = this.state.input;
-    const raw = JSON.stringify({
-      "user_app_id": {
-          "user_id": USER_ID,
-          "app_id": APP_ID
-      },
-      "inputs": [{"data": {"image": {"url": IMAGE_URL}}}]
-    });
-    
-    const requestOptions = {
-      method: 'POST',
-      headers: {
-          'Accept': 'application/json',
-          'Authorization': 'Key ' + PAT
-      }, body: raw
-    };
-    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
-      .then(response => response.text())
-      .then(response => {
-        const parser = JSON.parse(response)
-        console.log('hi', parser.outputs[0].data.regions[0].region_info.bounding_box)
-        // console.log(response[])
-        if (response) {
-          fetch('http://localhost:3000/image', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-              id: this.state.user.id
-            })
-          })
-          .then(response => response.json())
-          .then(count => {
-            this.setState(Object.assign(this.state.user, { entries: count }))
-          })
-        }
-        this.displayFaceBox(this.calculateFaceLocation(response))
-      })
-      .then(result => console.log(result))
-      .catch(error => console.log('error', error));
-
-  //andrei's
   // onButtonSubmit = () => {
+  //   console.log('button clicked');
   //   this.setState({imageUrl: this.state.input});
-  //     fetch('https://intense-plains-82472.herokuapp.com/imageurl', {
-  //       method: 'post',
-  //       headers: {'Content-Type': 'application/json'},
-  //       body: JSON.stringify({
-  //         input: this.state.input
-  //       })
-  //     })
-  //     .then(response => response.json())
+  //   const USER_ID = 'oxxxxxxxxxx';//(the code by your name)
+  //   const PAT = '96d20xxxxxxxxxxxx';//(your Clarifai api key)
+  //   const APP_ID = 'facerec';//(what you named your app in Clarifai)
+  //   const MODEL_ID = 'face-detection';
+  //   const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';    
+  //   const IMAGE_URL = this.state.input;
+  //   const raw = JSON.stringify({
+  //     "user_app_id": {
+  //         "user_id": USER_ID,
+  //         "app_id": APP_ID
+  //     },
+  //     "inputs": [{"data": {"image": {"url": IMAGE_URL}}}]
+  //   });
+    
+  //   const requestOptions = {
+  //     method: 'POST',
+  //     headers: {
+  //         'Accept': 'application/json',
+  //         'Authorization': 'Key ' + PAT
+  //     }, body: raw
+  //   };
+  //   fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
+  //     .then(response => response.text())
   //     .then(response => {
+  //       const parser = JSON.parse(response)
+  //       console.log('hi', parser.outputs[0].data.regions[0].region_info.bounding_box)
+  //       // console.log(response[])
   //       if (response) {
-  //         fetch('https://intense-plains-82472.herokuapp.com/image', {
+  //         fetch('http://localhost:3000/image', {
   //           method: 'put',
   //           headers: {'Content-Type': 'application/json'},
   //           body: JSON.stringify({
   //             id: this.state.user.id
   //           })
   //         })
-  //           .then(response => response.json())
-  //           .then(count => {
-  //             this.setState(Object.assign(this.state.user, { entries: count}))
-  //           })
-  //           .catch(console.log)
-
+  //         .then(response => response.json())
+  //         .then(count => {
+  //           this.setState(Object.assign(this.state.user, { entries: count }))
+  //         })
   //       }
   //       this.displayFaceBox(this.calculateFaceLocation(response))
   //     })
-  //     .catch(err => console.log(err));
-  // }
+  //     .then(result => console.log(result))
+  //     .catch(error => console.log('error', error));
+
+  // andrei's
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+      fetch('https://intense-plains-82472.herokuapp.com/imageurl', {
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+          input: this.state.input
+        })
+      })
+      .then(response => response.json())
+      .then(response => {
+        if (response) {
+          fetch('https://intense-plains-82472.herokuapp.com/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, { entries: count}))
+            })
+            .catch(console.log)
+
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
+      .catch(err => console.log(err));
+  }
 
   // onButtonSubmit = () => {
   //   this.setState({imageUrl: this.state.input});
